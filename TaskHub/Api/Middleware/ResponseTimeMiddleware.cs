950bb6a4ded6fbe856e642b11ce8c9d50e0fbd22
Софья Матвеева <soniam080604@gmail.com>
2026-02-27 01:31:41 +0500

    Add middleware

---
+++ TaskHub/Api/Middleware/ResponseTimeMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace Api.Middleware
+{
+    public class ResponseTimeMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ResponseTimeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var start = DateTime.UtcNow;
+
+            var originalBody = context.Response.Body;
+            using var memStream = new MemoryStream();
+            context.Response.Body = memStream;
+
+            try
+            {
+                await _next(context);
+
+                var elapsedMs = (DateTime.UtcNow - start).TotalMilliseconds;
+
+                context.Response.Headers.TryAdd("X-Response-Time-Ms", elapsedMs.ToString("F0"));
+
+                memStream.Seek(0, SeekOrigin.Begin);
+                await memStream.CopyToAsync(originalBody);
+            }
+            finally
+            {
+                context.Response.Body = originalBody;
+            }
+        }
+    }
+}
---
+++ TaskHub/Api/Middleware/StudentInfoMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace Api.Middleware
+{
+    public class StudentInfoMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public StudentInfoMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.Headers.TryAdd("X-Student-Name", "Matveeva Sonia Vadimovna");
+            context.Response.Headers.TryAdd("X-Student-Group", "RI-240912");
+
+            await _next(context);
+        }
+    }
+}
